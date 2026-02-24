c12a31b8b96b9ad43021edf72a371ded9c5a1d31
AlphaMikey23 <eng22cse001@gmail.com>
2026-02-24 13:01:57 +0530

    Added Conditional style rendering

--- src/RenderingLists.jsx
+++ src/RenderingLists.jsx
@@ -1,19 +1,19 @@
 export default function RenderingLists() {
     const products = [
-        {id: 1,title: 'Cabbage'},
-        {id: 2,title: 'Onion'},
-        {id: 3,title: 'drumstick'}
+        {id: 1,title: 'Cabbage',isFruit: false},
+        {id: 2,title: 'Onion',isFruit: false},
+        {id: 3,title: 'Apple',isFruit: true}
     ]
 
     const listItems = products.map(product => 
-        <li key={product.id}> {product.title}</li>
+        <li key={product.id} style={{color: product.isFruit ? 'green' : 'white'}}> {product.title}</li>
     )
 
     // console.log(products.map(product => <li key={product.id}>{product.title}</li>));
 
     return(
         <>
-        <ul>{listItems}</ul>
+        <ul >{listItems}</ul>
         </>
     )
 }
